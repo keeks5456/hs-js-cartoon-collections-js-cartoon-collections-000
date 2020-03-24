2ab637dd47b74e50b94b6cd562d63b498a294f6b
Akiko Green <akikogreen08@gmail.com>
2020-03-24 20:00:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function dwarfRollCall(dwarves) {
-}
 
+function dwarfRollCall(dwarves) {
+  let helper = []
+for(let i = 0; i < dwarves.length; i++){
+//push() at every index of dwarves add up by one  
+  helper.push(`${i+1}. ${dwarves[i]} `)
+}
+//dont forget join() puts the array back together into a full line string
+return helper.join("")
+  }
+
+//use .upperCase()
+  //concat exclamation points at end
+  
 function summonCaptainPlanet(planeteerCalls){
+  let i = 0;
+  let calls = []
+  while (i < planeteerCalls.length){
+    calls.push(planeteerCalls[i].toUpperCase() + "!")
+    i++;
+  }
+  return calls;
 }
 
 function longPlaneteerCalls(words) {
+if(words.length > 4){
+  return true
+}
+return false
 }
 
 function findTheCheese (foods) {
+  let i = 0;
+  do {
+    foods.findIndex() === foods[i]
+    i++;
+  }
+  while(i < foods.length)
 }
